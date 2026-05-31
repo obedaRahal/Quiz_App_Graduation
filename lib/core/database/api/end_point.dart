61eddef935f3fdbd83ad557a0d0f6fe791143b7b
carmen sh <carmenalshoufi8@gmail.com>
2026-05-31 15:22:46 +0300

    Fix Android build issue and update app links dependencies

--- lib/core/database/api/end_point.dart
+++ lib/core/database/api/end_point.dart
@@ -2,8 +2,8 @@ import 'package:quiz_app_grad/features/details_of_test/presentation/manager/test
 
 class EndPoints {
   //static const String baseUrl = 'http://localhost/api/v1/user-mobile';
-  static const String baseUrl = 'http://localhost/api/v1/user-mobile';
-  static const String refreshToken = 'http://localhost/api/v1/refresh';
+  static const String baseUrl = 'http://192.168.138.1/api/v1/user-mobile';
+  static const String refreshToken = 'http://192.168.138.1/api/v1/refresh';
   // authhhhhh
   //static const String registerCitizen = 'citizen/register';
 
